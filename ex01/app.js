eb4546fe437b23deced2d3b44ef74f2eaa698638
Sektor30 <sektor9130@gmail.com>
2024-07-09 16:36:26 -0500

    ex 01

---
+++ ex01/app.js
@@ -0,0 +1,13 @@
+const entrada = document.getElementById('envio'); 
+const boton = document.getElementById('btn');
+const pelota = document.getElementById('circulo'); 
+
+
+
+boton.addEventListener('click', function() {
+    circulo.style.marginLeft = circulo.style.marginLeft === '300px' ? '0px' : '300px';
+    circulo.style.transitionDuration = '.10s';
+    circulo.style.backgroundColor = entrada.value === entrada.value ?  : ;
+
+    
+});
