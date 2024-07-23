a073cbeb9da248602e72fab2d715dee458bfc3cd
Sektor30 <sektor9130@gmail.com>
2024-07-23 15:37:06 -0500

    clase 23 julio 3

---
+++ 04-lista-de-tareas/app.js
@@ -0,0 +1,16 @@
+const main = document.querySelector('#main');
+const btncrear = document.querySelector('#btncrear');
+
+function crear(){
+    const marco = document.createElement('div');
+    const equis = document.createElement('button');
+
+    
+
+
+
+    equis.innerText = 'x';
+    equis.classList.add('equis')
+  }
+
+    
