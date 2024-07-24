83258e0dd58fb0e7f549a180d914bd3c8c9f6c60
Sektor30 <sektor9130@gmail.com>
2024-07-24 16:05:35 -0500

    lista tareas 4

--- 04-lista-de-tareas/app.js
+++ 04-lista-de-tareas/app.js
@@ -1,16 +1,45 @@
 const main = document.querySelector('#main');
 const btncrear = document.querySelector('#btncrear');
+const input = document.querySelector('input');
+
+
+
+btncrear.addEventListener('click', (e)=>{
+  e.preventDefault(); 
+    const text = input.value.trim();
+
+    console.log(text)
+
+  const li =document.createElement('li')
+  const p =document.createElement('p') 
+
+  p.textContent = text;
+
+  li.appendChild(p);
+  ul.appendChild(li); 
+
+});
+
+
+
+
+
+
+
+
+
 
 function crear(){
-    const marco = document.createElement('div');
-    const equis = document.createElement('button');
-
-    
+    /* const marco = document.createElement('div');
+    const equis = document.createElement('h1');
 
 
 
     equis.innerText = 'x';
-    equis.classList.add('equis')
+    equis.classList.add('equis') */
+
+
+
   }
 
     
