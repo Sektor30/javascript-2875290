7a1cf0324c76f4e91154c20ad5410e209a0a2673
Sektor30 <sektor9130@gmail.com>
2024-07-25 08:34:55 -0400

    list changes 3

--- 04-lista-de-tareas/app.js
+++ 04-lista-de-tareas/app.js
@@ -1,45 +1,57 @@
+const input = document.querySelector("input");
 const main = document.querySelector('#main');
 const btncrear = document.querySelector('#btncrear');
-const input = document.querySelector('input');
+const btnBorrar = document.querySelector('h1');
+const ul =  document.querySelector("ul");
+
+let n = 1;
 
 
-
-btncrear.addEventListener('click', (e)=>{
+btncrear.addEventListener('click', (e) => {
   e.preventDefault(); 
-    const text = input.value.trim();
+    const text = input.value;
 
-    console.log(text)
-
-  const li =document.createElement('li')
-  const p =document.createElement('p') 
+  const li = document.createElement('li')
+  const p = document.createElement('p') 
+  const equis =  document.createElement('h1')
 
   p.textContent = text;
 
+   equis.innerText = 'X';
+  equis.classList.add('button');
+
+
   li.appendChild(p);
+  li.appendChild(equis)
   ul.appendChild(li); 
 
 });
 
+function borrarpantalla(){
+  main.innerHTML='';
+  n=0;
+}
 
+function borrarElement(e){
+  /* console.log('le di click a la pelotica' +' '+ e.target.innerText)
+  main.removeChild(e.target) */
 
-
-
-
-
-
-
-
-function crear(){
-    /* const marco = document.createElement('div');
-    const equis = document.createElement('h1');
-
-
-
-    equis.innerText = 'x';
-    equis.classList.add('equis') */
-
-
-
+  if(e.target.innerHTML=='X'){
+    li.removeChild(e.target.parentElement) 
+    console.log('borrar equis')
   }
+ 
+}
+
+btnBorrar.addEventListener('click', borrarpantalla)
+main.addEventListener('click', borrarElement)
+
+
+
+
+
+
+
+
 
     
