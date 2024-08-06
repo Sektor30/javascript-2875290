993628325c7a515dca9e4d1c49fa60237900b6aa
Sektor30 <sektor9130@gmail.com>
2024-08-06 16:14:52 -0500

    data 5-3

--- 07-inicio-sesion/js/index.js
+++ 07-inicio-sesion/js/index.js
@@ -1,2 +1,32 @@
 console.log('ejecutando')
 
+const formulario = document.querySelector('.formuluario')
+const username = document.querySelector('.username')
+const password = document.querySelector('.password')
+
+
+
+
+function validarUsuario (e){
+    e.preventDefault(); 
+
+    let currentUser = JSON.parse( localStorage.getItem('user') )
+    console.log(currentUser.userName)
+    console.log(currentUser.userPass)
+
+    console.log(Boolean(username.value === currentUser.userName)) ;
+    console.log(Boolean(password.value === currentUser.userPass)) ;
+
+    if(username.value === currentUser.userName && password.value === currentUser.userPass){
+        console.log('Felicidades puedes entrar 🔥')
+        window.location = "../userpages.html"
+    }else{
+        console.log('Sigue intentando')
+    }
+
+    formulario.reset()
+}
+
+ formulario.addEventListener('submit', validarUsuario)  
+
+
--- 07-inicio-sesion/js/userPages.js
+++ 07-inicio-sesion/js/userPages.js
@@ -1,1 +1,7 @@
 console.log('ejecutando')
+
+const saludo = document.querySelector('#saludo');
+
+let nombreUser = JSON.parse( localStorage.getItem('user') );
+
+saludo.innerHTML = `Hola ${nombreUser.userName} 🍊🍍`
