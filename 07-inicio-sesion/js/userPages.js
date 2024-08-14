310071fcddae1d03f2ad9581a6824383f55b6d7d
Sektor30 <sektor9130@gmail.com>
2024-08-13 22:17:13 -0400

    fetch folders

--- 07-inicio-sesion/js/userPages.js
+++ 07-inicio-sesion/js/userPages.js
@@ -1,1 +1,10 @@
 console.log('ejecutando')
+
+
+const saludo = document.querySelector('#saludo');
+const container = document.querySelector('.contenedor')
+
+
+let nombreUser = JSON.parse( localStorage.getItem('user') );
+
+saludo.innerHTML = `Hola ${nombreUser.userName} 🎦`
