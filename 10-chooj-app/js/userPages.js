27c470f980e6a9abd50c56450117862c1de9e5bb
Sektor30 <sektor9130@gmail.com>
2024-08-26 16:30:16 -0400

    saves changes

---
+++ 10-chooj-app/js/userPages.js
@@ -0,0 +1,16 @@
+console.log('ejecutando')
+
+
+const saludo = document.querySelector('#saludo');
+const container = document.querySelector('.contenedor')
+
+
+let nombreUser = JSON.parse( localStorage.getItem('user') );
+
+saludo.innerHTML = `Hola ${nombreUser.userName} 🎦`
+
+
+
+
+
+
